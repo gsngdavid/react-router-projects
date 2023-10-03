b309a40adca9990cb7d3c0972b78c96c0bf12f50
gsngdavid <gsngdavid@gmail.com>
2023-10-03 07:44:22 +0200

    feat: Add home, about components and routes to them

--- vans-life/src/App.jsx
+++ vans-life/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
 function App() {
-  return <h1 className="text-center">Hello world</h1>
+  return <BrowserRouter>
+    <div className="page-container">
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+</BrowserRouter>
 }
 
 export default App
--- vans-life/src/main.jsx
+++ vans-life/src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import About from './About.jsx'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
